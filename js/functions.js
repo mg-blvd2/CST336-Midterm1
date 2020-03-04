c475431d34501c8d2d59f3e0e9652b58c1eccb7a
mg-blvd2 <mguijarro@csumb.edu>
2020-03-04 15:45:47 -0800

    Added most of the webpage

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,30 @@
-alert("Connected");
+function getBookInfo(){
+  $.ajax({
+    method:"GET",
+    url:"https://openlibrary.org/api/books",
+    data:{
+      "bibkeys" : "ISBN:" + $("#isbnNumber").val(),
+      "format" : "json",
+      "jscmd" : "data"
+    },
+    success : function(data){
+      addInfo(data[`ISBN:${$("#isbnNumber").val()}`]);
+    },
+    error: function(error){
+      console.log(error);
+    }
+  })
+}
+
+function addInfo(data){
+  console.log(data);
+  $("#bookInfo").html(`<img class="col-sm-3" src="${data["cover"]["medium"]}">`);
+  $("#bookInfo").append('<div class="col-sm-6 infoText">');
+  $(".infoText").html(`<p>Title: ${data.title}</p>`);
+  $(".infoText").append(`<p>Author: ${data.authors[0].name}</p>`);
+  $(".infoText").append(`<p>Publish Year: ${data.publish_data}</p>`);
+  $(".infoText").append(`<p>Publisher: ${data.publishers[0].name}</p>`);
+  $(".infoText").append(`<p>ISBN: ${$("#isbnNumber").val()}</p>`);
+  $(".infoText").append(`<p>Pages: ${data.number_of_pages}</p>`);
+  $(".infoText").append("</div>")
+}
